9079486504e902b8a23637bd56e4374621c09e16
pe200012 <1326263755@qq.com>
2022-02-16 12:57:49 +0800

    add files

---
+++ .emacs
@@ -0,0 +1,232 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-auto-complete nil)
+ '(company-auto-complete-chars '(32 41 46))
+ '(company-backends
+   '(company-yasnippet company-dabbrev-code company-robe company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+     (company-dabbrev-code company-gtags company-etags company-keywords)
+     company-oddmuse company-dabbrev))
+ '(company-box-doc-delay 0.1)
+ '(company-box-icons-alist 'company-box-icons-all-the-icons)
+ '(company-idle-delay 0.1)
+ '(company-lsp-cache-candidates 'auto)
+ '(company-minimum-prefix-length 3)
+ '(company-tooltip-align-annotations t)
+ '(custom-enabled-themes '(leuven))
+ '(custom-safe-themes
+   '("a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default))
+ '(default-input-method nil)
+ '(global-company-mode t)
+ '(haskell-font-lock-symbols t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type 'stack-ghci)
+ '(haskell-stylish-on-save t)
+ '(haskell-tags-on-save t)
+ '(ido-enable-flex-matching t)
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice t)
+ '(initial-major-mode 'lisp-mode)
+ '(lsp-enable-semantic-highlighting nil)
+ '(lsp-file-watch-threshold 1000000)
+ '(lsp-ui-flycheck-enable t)
+ '(package-archives
+   '(("org" . "https://orgmode.org/elpa/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+ '(package-selected-packages
+   '(proof-general company-coq merlin-eldoc fish-mode magit corral slime-company js2-mode jsx-mode web-mode tide inf-ruby lsp-treemacs company-lsp lsp-ui lsp-mode ivy doom-modeline rubocop seeing-is-believing ruby-electric ruby-tools yari company-box doom-themes company-quickhelp flycheck-rtags cpputils-cmake lsp-haskell skewer-mode emmet-mode ccls lsp-ui-peek lsp-ui-sideline cmake-mode flymake-hlint intero hasktags helm-lsp mode-icons hindent rvm robe projectile enh-ruby-mode cmake-ide protobuf-mode flycheck-ocaml irony-eldoc flymake-yaml flycheck-yaml yaml-mode flycheck-yamllint highlight-indent-guides smart-cursor-color multiple-cursors company-lua lua-mode flymake-lua highlight-symbol rainbow-delimiters counsel monokai-theme neotree clang-format moonscript haskell-mode company-irony flycheck-irony flycheck irony utop tuareg emojify all-the-icons powerline elpy color-identifiers-mode all-the-icons-dired yasnippet-snippets yasnippet paradox smartparens line-number-display-width display-line-numbers-mode company company-mode smex use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq user-full-name "pe200012")
+(setq user-mail-address "1326263755@qq.com")
+(require 'cl-lib)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(setq recentf-max-saved-items 100)
+(setq recentf-max-menu-items 100)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(add-to-list 'default-frame-alist
+	     '(font . "Iosevka Moe-12"))
+(require 'package)
+(package-initialize)
+(setq wakatime-api-key "b5490808-fa87-460d-b8a8-aa2fa4cf7696")
+(setq wakatime-cli-path "/home/pe200012/.wakatime/wakatime-cli-linux-amd64")
+(load "~/.emacs-packages.el")
+(load "~/firacode.el")
+
+;;
+(setq highlight-indentation-blank-lines t)
+
+(add-hook 'after-init-hook
+	  (lambda ()
+	    (require 'smartparens-config)
+	    (smartparens-global-mode)
+	    (global-prettify-symbols-mode +1)
+	    ))
+
+(with-eval-after-load 'company
+;  (add-to-list 'company-backends 'merlin-company-backend)
+  (add-to-list 'company-backends 'company-robe)
+  (add-to-list 'company-backends 'company-dabbrev-code)
+  (add-to-list 'company-backends 'company-yasnippet))
+
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rvm-activate-corresponding-ruby))
+
+'(add-hook 'merlin-mode-hook 'company-mode)
+
+(defun haskell-interactive-toggle-print-mode ()
+  (interactive)
+  (setq haskell-interactive-mode-eval-mode
+        (intern
+         (ido-completing-read "Eval result mode: "
+                              '("fundamental-mode"
+                                "haskell-mode"
+                                "espresso-mode"
+                                "ghc-core-mode"
+                                "org-mode")))))
+
+(setq haskell-process-args-ghci
+          '("-ferror-spans" "-fshow-loaded-modules"))
+
+(setq haskell-process-args-cabal-repl
+          '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+
+(setq haskell-process-args-stack-ghci
+          '("--ghci-options=-ferror-spans -fshow-loaded-modules"
+            "--no-build" "--no-load"))
+
+(setq haskell-process-args-cabal-new-repl
+          '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl")
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; configure jsx-tide checker to run after your default jsx checker
+;;(flycheck-add-mode 'javascript-eslint 'web-mode)
+;;(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+
+(defun my-flycheck-rtags-setup ()
+  (flycheck-select-checker 'rtags)
+  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+  (setq-local flycheck-check-syntax-automatically nil))
+;; c-mode-common-hook is also called by c++-mode
+(add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
+
+;; Goto definition
+(defun ido-imenu ()
+  "Update the imenu index and then use ido to select a symbol to navigate to.
+Symbols matching the text at point are put first in the completion list."
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols
+            (symbol-list)
+            (when (listp symbol-list)
+              (dolist (symbol symbol-list)
+                (let ((name nil) (position nil))
+                  (cond
+                   ((and (listp symbol) (imenu--subalist-p symbol))
+                    (addsymbols symbol))
+
+                   ((listp symbol)
+                    (setq name (car symbol))
+                    (setq position (cdr symbol)))
+
+                   ((stringp symbol)
+                    (setq name symbol)
+                    (setq position
+                          (get-text-property 1 'org-imenu-marker symbol))))
+
+                  (unless (or (null position) (null name))
+                    (add-to-list 'symbol-names name)
+                    (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    ;; If there are matching symbols at point, put them at the beginning
+    ;; of `symbol-names'.
+    (let ((symbol-at-point (thing-at-point 'symbol)))
+      (when symbol-at-point
+        (let* ((regexp (concat (regexp-quote symbol-at-point) "$"))
+               (matching-symbols
+                (delq nil (mapcar
+                           (lambda (symbol)
+                             (if (string-match regexp symbol) symbol))
+                           symbol-names))))
+          (when matching-symbols
+            (sort matching-symbols (lambda (a b) (> (length a) (length b))))
+            (mapc
+             (lambda (symbol)
+               (setq symbol-names (cons symbol (delete symbol symbol-names))))
+             matching-symbols)))))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (push-mark)
+      (if (overlayp position)
+          (goto-char (overlay-start position))
+        (goto-char position)))))
+
+(global-set-key (kbd "C-x C-i") 'ido-imenu)
+;;
+
+;; agda setup
+;; auto-load agda-mode for .agda and .lagda.md
+
+(load-file (let ((coding-system-for-read 'utf-8))
+	     (shell-command-to-string "env agda-mode locate")))
+
+(setq auto-mode-alist
+   (append
+     '(("\\.agda\\'" . agda2-mode)
+       ("\\.lagda.md\\'" . agda2-mode))
+     auto-mode-alist))
+
+(provide '.emacs)
+;;; .emacs ends here
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
---
+++ .emacs-packages.el
@@ -0,0 +1,383 @@
+
+;;; leaf manager load
+
+;; <leaf-install-code>
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+      :ensure t
+      :init
+      ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+      (leaf hydra :ensure t)
+      (leaf el-get :ensure t)
+      (leaf blackout :ensure t)
+
+      :config
+      ;; initialize leaf-keywords.el
+      (leaf-keywords-init)))
+;; </leaf-install-code>
+
+(leaf leaf-tree :ensure t)
+(leaf leaf-convert :ensure t)
+(leaf transient-dwim
+    :ensure t
+    :bind (("M-=" . transient-dwim-dispatch)))
+
+(leaf feather
+    :el-get conao3/feather.el
+    :config
+    (feather-mode))
+
+(defcustom leaf-alias-keyword-alist '((:ensure . :feather))
+  "The alias keyword.  KEY is treated as an alias for VALUE."
+  :type 'sexp
+  :group 'leaf)
+
+;;; Packages
+;; ===== Appearance =====
+;; (leaf solarized-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'solarized-dark t))
+'(leaf monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t)
+  (setq monokai-user-variable-pitch t))
+(leaf highlight-symbol :ensure t)
+'(leaf powerline
+  :ensure t
+  :config
+  (powerline-default-theme))
+(leaf doom-themes
+    :ensure t
+    :config
+    (setq doom-themes-enable-bold t
+	  doom-themes-enable-italic t)
+    (load-theme 'doom-molokai t)
+    (doom-themes-visual-bell-config)
+    (doom-themes-neotree-config)
+    (doom-themes-org-config))
+(leaf doom-modeline
+    :ensure t
+    :hook (after-init-hook . doom-modeline-mode)
+    :config
+    (setq doom-modeline-icon (display-graphic-p))
+    (setq doom-modeline-major-mode-icon t)
+    (setq doom-modeline-major-mode-color-icon t)
+    (setq doom-modeline-buffer-state-icon t)
+    (setq doom-modeline-buffer-modification-icon t)
+    (setq doom-modeline-unicode-fallback t)
+    (setq doom-modeline-buffer-encoding t)
+    (setq doom-modeline-lsp t)
+    (setq doom-modeline-github nil)
+    (setq doom-modeline-env-version t)
+    (setq doom-modeline-env-load-string "...")
+    (setq doom-modeline-enable-word-count t))
+(leaf all-the-icons :ensure t)
+'(leaf emojify
+  :ensure t
+  :hook (after-init-hook . global-emojify-mode))
+(leaf color-identifiers-mode
+    :ensure t
+    :hook (after-init-hook . global-color-identifiers-mode))
+(leaf rainbow-delimiters
+    :ensure t
+    :hook (prog-mode-hook . rainbow-delimiters-mode))
+(leaf highlight-indentation
+    :hook (prog-mode-hook . highlight-indentation-current-column-mode))
+(leaf company-box
+    :ensure t
+    :hook (company-mode-hook . company-box-mode))
+
+;; ===== Project Management =====
+(leaf neotree
+    :ensure t
+    :init
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+(leaf dash
+  :ensure t)
+(leaf projectile
+    :ensure t
+    :hook (prog-mode-hook . projectile-mode))
+(leaf magit
+    :ensure t)
+
+;; ===== Completion & Diagnoise =====
+(leaf company
+    :ensure t
+    :config
+    (global-company-mode))
+(leaf yasnippet
+    :ensure t
+    :config
+    (yas-global-mode 1))
+(leaf yasnippet-snippets :ensure t)
+(leaf ivy
+    :ensure t
+    :init
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    (ivy-mode 1))
+(leaf counsel :ensure t)
+(leaf smex
+    :ensure t
+    :config
+    (smex-initialize)
+    (global-set-key (kbd "M-x") 'smex)
+    (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+    ;; This is your old M-x.
+    (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+(leaf flymake :ensure t)
+(leaf eldoc :ensure t)
+(leaf flycheck
+    :ensure t
+    :hook ((after-init-hook . global-flycheck-mode)
+	   (lsp-mode-hook   . flycheck-mode)
+	   (irony-mode-hook . irony-eldoc)
+	   ))
+
+;; ===== Language Specific =====
+;; ~~~ LSP ~~~
+(leaf lsp-mode
+    :ensure t
+    :hook ((haskell-mode-hook . lsp)
+	   (haskell-mode-hook . lsp-mode)
+	   (c++-mode-hook     . lsp)
+	   (c-mode-hook       . lsp-mode))
+    :config
+    (lsp-enable-imenu)
+    (setq lsp-file-watch-thresold nil))
+(leaf lsp-ui
+    :ensure t
+    :hook ((lsp-mode-hook     . lsp-ui-mode)
+	   (haskell-mode-hook . lsp-ui-mode)
+	   (c++-mode-hook     . lsp-ui-mode)
+	   (c-mode-hook       . lsp-ui-mode)))
+(leaf lsp-ui-sideline)
+(leaf lsp-ui-peek)
+(leaf company-lsp
+    :ensure t
+    :hook ((lsp-mode-hook . company-lsp))
+    :config
+    (push 'company-lsp company-backends)
+    (setq company-lsp-cache-candidates 'auto)
+    (setq company-lsp-async t)
+    (setq company-lsp-enable-snippet t)
+    (setq company-lsp-enable-recompletion t))
+(leaf lsp-treemacs
+    :ensure t
+    :commands lsp-treemacs-errors-list)
+(leaf helm-lsp
+    :ensure t
+    :commands helm-lsp-workspace-symbol)
+(leaf company-quickhelp
+    :ensure t)
+
+;; ~~~ Common Lisp ~~~
+(leaf slime-company
+    :ensure t)
+
+(leaf slime
+    :ensure t
+    :hook (slime-mode-hook
+	   . (lambda ()
+	       (unless (slime-connected-p)
+		 (save-excursion (slime)))))
+    :config
+    ;; Set your lisp system and, optionally, some contribs
+    (setq inferior-lisp-program "/opt/sbcl/bin/sbcl")
+    (setq slime-contribs '(slime-fancy slime-indentation))
+    (setq lisp-indent-function 'common-lisp-indent-function)
+    (setq common-lisp-style-default "modern")
+    (slime-setup '(slime-fancy slime-company)))
+
+;; ~~~ Coq ~~~
+(leaf company-coq
+    :ensure t
+    :hook (coq-mode-hook . company-coq-mode))
+
+(leaf proof-general
+    :ensure t)
+
+;; ~~~ Python ~~~
+(leaf python
+    :ensure t
+    :mode ("\\.py\\'" . python-mode)
+    :interpreter ("python" . python-mode))
+(leaf elpy
+    :ensure t
+					; :defer t
+    :init
+    (advice-add 'python-mode :before 'elpy-enable))
+
+;; ~~~ Lua ~~~
+(leaf flymake-lua :ensure t)
+(leaf lua-mode :ensure t)
+(leaf company-lua :ensure t)
+
+;; ~~~ Markdown ~~~
+(leaf markdown-mode
+    :ensure t)
+(leaf markdown-preview-eww
+    :ensure t)
+
+;; ~~~ Haskell ~~~
+(leaf haskell-mode
+    :ensure t
+    :config
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+    (add-hook 'haskell-mode-hook 'haskell-font-lock-symbols t)
+    (define-key haskell-mode-map (kbd "C-c C-h C-l") 'haskell-process-load-or-reload)
+    (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+					;  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+					;  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+					;  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+					;  (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+					;  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+					;  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+					;  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+					;  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+					;  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+    :hook ((haskell-mode-hook . interactive-haskell-mode)
+	   (haskell-mode-hook . company-mode)
+	   (haskell-mode-hook . intero-mode)
+	   (haskell-mode-hook . lsp-mode)))
+(leaf haskell-interactive-mode)
+(leaf haskell-process)
+(leaf flymake-hlint :ensure t)
+'(leaf intero
+  :ensure t
+  :config
+  (intero-global-mode 1))
+'(leaf lsp-haskell
+  :ensure t
+  :config
+  (lsp-haskell-set-hlint-on)
+  (lsp-haskell-set-completion-snippets-on)
+  (add-hook 'haskell-mode-hook #'lsp))
+(leaf hindent
+    :ensure t
+    :hook (haskell-mode-hook . hindent-mode)
+    :config
+    (setq exec-path `("/home/pe200012/.local/bin" ,@exec-path)))
+
+;; ~~~ OCaml ~~~
+(leaf utop :ensure t)
+
+;; ~~~ Moonscript ~~~
+(leaf moonscript :ensure t)
+
+;; ~~~ Yaml ~~~
+(leaf yaml-mode :ensure t)
+(leaf flymake-yaml :ensure t)
+
+;; ~~~ C & C++ ~~~
+;; (leaf irony
+;;   :ensure t
+;;   :hook ((c++-mode   . irony-mode)
+;; 	 (c-mode     . irony-mode)
+;; 	 (objc-mode  . irony-mode)
+;; 	 (irony-mode . irony-cdb-autosetup-compile-options)))
+;; (leaf flycheck-irony
+;;   :ensure t
+;;   :config
+;;   (eval-after-load 'flycheck
+;;     '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+;; (leaf company-irony
+;;   :ensure t
+;;   :config
+;;   (eval-after-load 'company
+;;     '(add-to-list 'company-backends 'company-irony)))
+;; (leaf clang-format
+;;  :ensure t)
+(leaf ccls
+    :ensure t
+    :hook ((c-mode-hook c++-mode-hook objc-mode-hook cuda-mode-hook) .
+	   (lambda () (require 'ccls) (lsp)))
+    :config
+    (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t) t))
+    (setq lsp-prefer-flymake nil)
+    (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+    (setq ccls-sem-highlight-method 'font-lock)
+    (ccls-use-default-rainbow-sem-highlight)
+    (define-key c++-mode-map (kbd "C-'") 'company-lsp))
+'(leaf flycheck-rtags
+  :ensure t)
+
+;; ~~~ Ruby ~~~
+(leaf ruby-mode)
+(leaf enh-ruby-mode
+    :ensure t
+    :config
+    (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+    (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+    (add-to-list 'auto-mode-alist
+		 '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+    )
+(leaf inf-ruby :ensure t)
+(leaf yari :ensure t)
+(leaf ruby-tools :ensure t)
+(leaf ruby-electric :ensure t)
+(leaf rvm
+    :ensure t
+    :config
+    (rvm-use-default))
+(leaf seeing-is-believing
+    :ensure t
+    :config
+    (setq seeing-is-believing-prefix "C-,"))
+(leaf rubocop :ensure t)
+(leaf robe
+    :ensure t
+    :config
+    (eval-after-load 'company
+      '(push 'company-robe company-backends)))
+
+;; ===== Web Development =====
+'(leaf tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode-hook . tide-setup)
+	 (typescript-mode-hook . tide-hl-identifier-mode)
+	 (before-save-hook . tide-format-before-save)
+	 (typescript-mode-hook . company-tide)))
+'(leaf web-mode :ensure t)
+'(leaf jsx-mode :ensure t)
+'(leaf js2-mode :ensure t)
+'(leaf emmet-mode
+  :ensure t
+  :hook (sgml-mode css-mode))
+'(leaf skewer-mode
+  :ensure t
+  :hook (js2-mode css-mode html-mode))
+'(leaf simple-httpd :ensure t)
+
+;; ===== Utils =====
+(leaf smartparens :ensure t)
+(leaf spinner :ensure t)
+(leaf paradox :ensure t)
+(leaf ido
+    :config
+  (ido-mode t))
+(leaf multiple-cursors :ensure t)
+(leaf smart-cursor-color :ensure t)
+(leaf wakatime-mode
+    :ensure t
+    :config
+        (global-wakatime-mode))
+
+
+(provide '.emacs-packages)
+;;; .emacs-packages ends here
+
+
